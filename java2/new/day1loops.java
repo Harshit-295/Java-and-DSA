88e7958faf6b48a8c14956b0f7256b39bffe66e2
Harshit-295 <bhardwajharsh1984@gmail.com>
2024-08-08 07:30:02 +0530

    Add files via upload

---
+++ java2/new/day1loops.java
@@ -0,0 +1,143 @@
+import java.util.Scanner;
+
+public class day1loops{
+    public static void main(String[] args) {
+    //     // Scanner input=new Scanner(System.in);
+    //     // int age=input.nextInt();
+    //     // System.out.println("age is "+age);
+
+    //     Scanner input=new Scanner(System.in);
+    //     int age=input.nextInt();
+    //     if(age>=18){
+    //         System.out.println("yOU ELIGLIBLE");
+    //     }
+    //     else{
+    //         System.out.println("not eligible");
+    //     }
+
+
+
+    /** Pattern  in java ***/
+    // triangle
+    // Scanner input=new Scanner(System.in);
+    // int noofstars=input.nextInt();
+    // for(int i=1;i<=noofstars;i++){
+    //     for(int j=1;j<=i;j++){
+    //         System.out.print("*");
+    //     }
+    // System.out.println();   
+    // }
+
+    //inverted tringle
+    // Scanner input=new Scanner(System.in);
+    // int n=input.nextInt();
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=n-i+1;j++){
+    //         System.out.print("*");
+    //     }
+    //     System.out.println();
+    // }
+
+    // no pyramid
+
+    // Scanner input=new Scanner(System.in);
+    // int n=input.nextInt();
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=i;j++){
+    //         System.out.print(j);
+    //     }
+    // System.out.println();
+    // }
+
+    // Hollow Rectangle
+    // Scanner input=new Scanner(System.in);
+    // int n=input.nextInt();
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=n;j++){
+    //         if(i==1||j==1||i==n||j==n){
+    //             System.out.print("*");
+    //         }
+    //         else{
+    //             System.out.print(" ");
+    //         }
+        
+    //     }
+    //     System.out.println();
+    // }
+
+
+    /// ulta adha triagle
+
+    // Scanner input=new Scanner(System.in);
+    // int n=input.nextInt();
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=n-i+1;j++){
+    //         System.out.print(" ");
+    //     }
+    //     for( int j=1;j<=i;j++){
+    //         System.out.print("*");
+    //     }
+    //     for(int j=2;j<=i;j++){
+    //         System.out.print("*");
+    //     }
+    //     System.out.println();
+    // }
+
+    //Inverted half pyramid with no
+
+    // Scanner input=new Scanner(System.in);
+    // int n=input.nextInt();
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=i-1;j++){
+    //         System.out.print(" ");
+    //     }
+    //     for(int j=1;j<=n-i+1;j++){
+    //         System.out.print(j);
+    //     }
+    //     for(int j=1;j<n-i+1;j++){
+    //         System.out.print(j);
+    //     }
+    //     System.out.println();
+    // }
+
+
+    // 01 triangle
+    // Scanner input=new Scanner(System.in);
+    // int n=input.nextInt();
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=i;j++){
+    //         if((i+j)%2==0){
+    //             System.out.print("1");
+    //         }
+    //         else{
+    //             System.out.print("0");
+    //         }
+    //     }
+    //     System.out.println();
+    // }
+
+
+
+
+//// Floyds triangle
+     
+    // Scanner input=new Scanner(System.in);
+    // int n=input.nextInt();
+    // int c=1;
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=i;j++){
+    //         System.out.print(c+" ");
+    //         c++;
+    //     }
+    //     System.out.println();
+    // } 
+
+
+    
+
+
+
+    
+
+    }    
+}
