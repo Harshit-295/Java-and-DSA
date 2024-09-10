f7f60725399f528a36adce5836b389b8142362bd
Harshit-295 <bhardwajharsh1984@gmail.com>
2024-09-10 17:37:56 +0530

    Update binarysearch.java

--- searching/binarysearch.java
+++ searching/binarysearch.java
@@ -29,41 +29,48 @@ public class binarysearch {
     //find the array is sorted in ascending or not
 
 
-    public static int Orderdiagonstics(int arr[],int target){
-        int start=0,end=arr.length-1;
-        boolean isArr=arr[start]<arr[end];
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            //int mid=(start+end)/2;// it may exced by the range //
-            if(isArr){
-                if(target>arr[mid]){
-                    start=mid+1;
-                }
-                else if(target<arr[mid]){
-                    end=mid-1;
-                }
-            }
-            else{
-                if(target<arr[mid]){
-                    start=mid+1;
-                }
-                else if(target>arr[mid]){
-                    end=mid-1;
-                }
-            }
-            if(target==arr[mid]){
-               return mid;
-            }
+    // public static int Orderdiagonstics(int arr[],int target){
+    //     int start=0,end=arr.length-1;
+    //     boolean isArr=arr[start]<arr[end];
+    //     while(start<=end){
+    //         int mid=start+(end-start)/2;
+    //         //int mid=(start+end)/2;// it may exced by the range //
+    //         if(isArr){
+    //             if(target>arr[mid]){
+    //                 start=mid+1;
+    //             }
+    //             else if(target<arr[mid]){
+    //                 end=mid-1;
+    //             }
+    //         }
+    //         else{
+    //             if(target<arr[mid]){
+    //                 start=mid+1;
+    //             }
+    //             else if(target>arr[mid]){
+    //                 end=mid-1;
+    //             }
+    //         }
+    //         if(target==arr[mid]){
+    //            return mid;
+    //         }
             
-        } 
-        return -1;  
-    }
-    public static void main(String[] args) {
-        int arr[]={10,8,6,4,2};
-        int target=6;
-        int ans=Orderdiagonstics(arr, target);
-        System.out.println(ans);
-    }
+    //     } 
+    //     return -1;  
+    // }
+    // public static void main(String[] args) {
+    //     int arr[]={10,8,6,4,2};
+    //     int target=6;
+    //     int ans=Orderdiagonstics(arr, target);
+    //     System.out.println(ans);
+    // }
+
+
+
+   public static void main(String[] args){
+       System.out.println("Harshit");
+
+    
 
     
 
